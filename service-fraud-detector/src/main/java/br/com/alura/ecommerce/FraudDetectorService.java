51aafa0d8363af9570533276d80fd079d34e2bf6
Mateus Barbosa <mateuscbarbosa@gmail.com>
2023-11-09 08:19:02 -0300

    refactor: serviço de fraudes usando banco de dados

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,30 +1,43 @@
 package br.com.alura.ecommerce;
 
 import java.math.BigDecimal;
-import java.util.Map;
+import java.sql.SQLException;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
+import br.com.alura.ecommerce.database.LocalDatabase;
 import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
 
-public class FraudDetectorService {
+public class FraudDetectorService implements ConsumerService<Order>{
 
 	private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-	
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		var fraudService = new FraudDetectorService();
-		try (var service = new KafkaService(
-				FraudDetectorService.class.getSimpleName(),
-				"ECOMMERCE_NEW_ORDER",
-				fraudService::parse,
-				Map.of())){
-			service.run();
-		}
+	private final LocalDatabase database;
+
+	FraudDetectorService() throws SQLException{
+		this.database = new LocalDatabase("ecommerce_frauds");
+		this.database.crateIfNotExists("CREATE TABLE IF NOT EXISTS orders("
+			+ "uuid VARCHAR(255) PRIMARY KEY,"
+			+ "is_fraud BOOLEAN)");
+	}
+	public static void main(String[] args) {
+		new ServiceRunner<>(FraudDetectorService::new).start(1);
 	}
 	
-	private void parse(ConsumerRecord<String, Message<Order>> record) throws InterruptedException, ExecutionException {
+	@Override
+	public String getTopic() {
+		return "ECOMMERCE_NEW_ORDER";
+	}
+	
+	@Override
+	public String getConsumerGroup() {
+		return FraudDetectorService.class.getSimpleName();
+	}
+	
+	@Override
+	public void parse(ConsumerRecord<String, Message<Order>> record) throws InterruptedException, ExecutionException, SQLException {
 		System.out.println("------------------------------------------");
 		System.out.println("Processing new order, checking for frauds.");
 		System.out.println(record.key());
@@ -33,14 +46,23 @@ public class FraudDetectorService {
 		System.out.println(record.offset());
 		
 		var message = record.value();
+		var order = message.getPayload();
+		
+		if(wasProcessed(order)) {
+			System.out.printf("""
+					Order %s was alredy processed.
+					""", order.getOrderId());
+			return;
+		}
+		
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		var order = message.getPayload();
 		if(isFraud(order)) {
-			//pretending that the fraud happens when the amount is < 1000
+			//pretending that the fraud happens when the amount is > 4000
+			database.update("INSERT INTO orders (uuid, is_fraud) VALUES(?, true)", order.getOrderId());
 			System.out.println("Order is a fraud. " + order);
 			orderDispatcher.send(
 					"ECOMMERCE_ORDER_REJECTED",
@@ -49,6 +71,7 @@ public class FraudDetectorService {
 					order);
 		}
 		else {
+			database.update("INSERT INTO orders (uuid, is_fraud) VALUES(?, false)", order.getOrderId());
 			System.out.println("Approved: " + order);
 			orderDispatcher.send(
 					"ECOMMERCE_ORDER_APPROVED",
@@ -58,6 +81,13 @@ public class FraudDetectorService {
 		}
 	}
 
+	private boolean wasProcessed(Order order) throws SQLException {
+		var results = database.query("SELECT uuid FROM orders "
+				+ "WHERE uuid = ? "
+				+ "LIMIT 1", order.getOrderId());
+		
+		return results.next();
+	}
 	private boolean isFraud(Order order) {
 		return order.getAmount().compareTo(new BigDecimal("4000")) >= 0; 
 	}
